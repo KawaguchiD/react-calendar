93cdb08e938087b3708f4b79ab497d2cbe78d7f2
KawaguchiD <hirokey.0823@gmail.com>
2020-03-10 17:11:46 +0900

    hoge

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Button from '@material-ui/core/Button';
 import BasicTextFields from './time-pickers/TextFields';
 function App() {
   return (
+    <>
     <Button variant="contained" color="primary">
         Hello World
     </Button>
     <BasicTextFields />
+    </>
   );
 }
 
