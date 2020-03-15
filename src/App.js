014acb21f4f5c01a4d83961b11d9319536258213
KawaguchiD <hirokey.0823@gmail.com>
2020-03-16 00:58:38 +0900

    modal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import BasicTextFields from './time-pickers/TextFields';
-import MaterialUIPickers from './calendar/caledar';
+//import calendar from './calendar/caledar';
+//import modal from './time-pickers/modal';
+import TransitionsModal from './time-pickers/modal';
 function App() {
   return (
     <>
     <Button variant="contained" color="primary">
         Hello World
     </Button>
-    <BasicTextFields />
-    <MaterialUIPickers />
+    <TransitionsModal />
     </>
   );
 }
